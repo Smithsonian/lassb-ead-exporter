e9fefa947afd0affd53aa6d172bef88b9fa4ee78
Mark <fordmadox@gmail.com>
2023-05-04 16:43:03 -0400

    now with updates for the new agent module

---
+++ backend/lib/si_export_helpers.rb
@@ -0,0 +1,92 @@
+module ASpaceExport
+  # Convenience methods that will work for resource
+  # or archival_object models during serialization
+
+  module ArchivalObjectDescriptionHelpers
+
+    # MODIFICATION: add cultural_context as a subject, rather than geogname, which is ASpace's default.
+    def controlaccess_subjects
+      unless @controlaccess_subjects
+        results = []
+        linked = self.subjects || []
+        linked.each do |link|
+          subject = link['_resolved']
+
+          node_name = case subject['terms'][0]['term_type']
+                      when 'function'; 'function'
+                      when 'genre_form', 'style_period';  'genreform'
+                      when 'geographic'; 'geogname'
+                      when 'occupation';  'occupation'
+                      when 'topical', 'temporal','cultural_context'; 'subject'
+                      when 'uniform_title'; 'title'
+                      else; nil
+                      end
+
+          next unless node_name
+
+          content = subject['terms'].map{|t| t['term']}.join(' -- ')
+
+          atts = {}
+          atts['source'] = subject['source'] if subject['source']
+          atts['authfilenumber'] = subject['authority_id'] if subject['authority_id']
+          atts['altrender'] = subject['terms'][0]['term_type'] if subject['terms'][0]['term_type']
+
+          results << {:node_name => node_name, :atts => atts, :content => content}
+        end
+
+        @controlaccess_subjects = results
+      end
+
+      @controlaccess_subjects
+    end
+
+    # MODIFICATION: use new agent record identifier field for authfilenumber AND source, rather than agent name authority ID value, which is still ASpace's default even in 3.x, for whatever reason.
+    # Should also fix that strange 'fmo' issue, but later.
+    def controlaccess_linked_agents(include_unpublished = false)
+      unless @controlaccess_linked_agents
+        results = []
+        linked = self.linked_agents || []
+        linked.each_with_index do |link, i|
+          next if link['role'] == 'creator' || (link['_resolved']['publish'] == false && !include_unpublished)
+          role = link['relator'] ? link['relator'] : (link['role'] == 'source' ? 'fmo' : nil)
+
+          agent = link['_resolved'].dup
+          sort_name = agent['display_name']['sort_name']
+          rules = agent['display_name']['rules']
+          # NEW, begin
+          source = agent['agent_record_identifiers'].select {|i| i['primary_identifier'] == true}.map {|s| s['source']}.first
+          authfilenumber = agent['agent_record_identifiers'].select {|i| i['primary_identifier'] == true}.map {|ri| ri['record_identifier']}.first
+          # NEW, end
+          content = sort_name.dup
+
+          if link['terms'].length > 0
+            content << " -- "
+            content << link['terms'].map {|t| t['term']}.join(' -- ')
+          end
+
+          node_name = case agent['agent_type']
+                      when 'agent_person'; 'persname'
+                      when 'agent_family'; 'famname'
+                      when 'agent_corporate_entity'; 'corpname'
+                      when 'agent_software'; 'name'
+                      end
+
+          atts = {}
+          atts[:role] = role if role
+          atts[:source] = source if source
+          atts[:rules] = rules if rules
+          atts[:authfilenumber] = authfilenumber if authfilenumber
+          atts[:audience] = 'internal' if link['_resolved']['publish'] == false
+
+          results << {:node_name => node_name, :atts => atts, :content => content}
+        end
+
+        @controlaccess_linked_agents = results
+      end
+
+      @controlaccess_linked_agents
+    end
+
+
+  end
+end
---
+++ backend/model/si_ead_exporter.rb
@@ -0,0 +1,79 @@
+# encoding: utf-8
+require 'nokogiri'
+require 'securerandom'
+
+
+class EADSerializer < ASpaceExport::Serializer
+    serializer_for :ead
+
+    # MODIFCATION: Add @type for EDAN object_type processing
+    def serialize_extents(obj, xml, fragments)
+      if obj.extents.length
+        obj.extents.each do |e|
+          next if e["publish"] === false && !@include_unpublished
+          audatt = e["publish"] === false ? {:audience => 'internal'} : {}
+
+          extent_number_float = e['number'].to_f
+          extent_type = I18n.t('enumerations.extent_extent_type.'+e['extent_type'], :default => e['extent_type'])
+
+
+          xml.physdesc({:altrender => e['portion']}.merge(audatt)) {
+            if e['number'] && e['extent_type'] 
+              xml.extent({:type => extent_type}) {
+                sanitize_mixed_content("#{e['number']} #{extent_type}", xml, fragments)
+              }
+            end
+            if e['container_summary']
+              xml.extent({:altrender => 'carrier'}) {
+                sanitize_mixed_content( e['container_summary'], xml, fragments)
+              }
+            end
+            xml.physfacet { sanitize_mixed_cDontent(e['physical_details'],xml, fragments) } if e['physical_details']
+            xml.dimensions  {   sanitize_mixed_content(e['dimensions'],xml, fragments) }  if e['dimensions']
+          }
+        end
+      end
+    end
+
+    # MODIFICATION: use new agent record identifier field for authfilenumber AND source, rather than agent name authority ID value, which is still ASpace's default even in 3.x, for whatever reason.
+    # Should update this so that the code is modularized, but not right now.
+    def serialize_origination(data, xml, fragments)
+      unless data.creators_and_sources.nil?
+        data.creators_and_sources.each do |link|
+          agent = link['_resolved']
+          published = agent['publish'] === true
+  
+          next if !published && !@include_unpublished
+  
+          link['role'] == 'creator' ? role = link['role'].capitalize : role = link['role']
+          relator = link['relator']
+          sort_name = agent['display_name']['sort_name']
+          rules = agent['display_name']['rules']
+          # NEW, begin
+          source = agent['agent_record_identifiers'].select {|i| i['primary_identifier'] == true}.map {|s| s['source']}.first
+          authfilenumber = agent['agent_record_identifiers'].select {|i| i['primary_identifier'] == true}.map {|ri| ri['record_identifier']}.first
+          # NEW, end
+          node_name = case agent['agent_type']
+                      when 'agent_person'; 'persname'
+                      when 'agent_family'; 'famname'
+                      when 'agent_corporate_entity'; 'corpname'
+                      when 'agent_software'; 'name'
+                      end
+  
+          origination_attrs = {:label => role}
+          origination_attrs[:audience] = 'internal' unless published
+          xml.origination(origination_attrs) {
+            atts = {:role => relator, :source => source, :rules => rules, :authfilenumber => authfilenumber}
+            atts.reject! {|k, v| v.nil?}
+  
+            xml.send(node_name, atts) {
+              sanitize_mixed_content(sort_name, xml, fragments )
+            }
+          }
+        end
+      end
+    end
+  
+
+end
+
